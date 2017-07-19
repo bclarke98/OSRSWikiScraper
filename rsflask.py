2def24af730629d2e4f6b249a58af72838dc9431
bclarke98 <bclarke98@outlook.com>
2017-07-19 11:49:13 -0400

    Added command line support. Use -h or --help for a list of parameters

--- rsflask.py
+++ rsflask.py
@@ -1,35 +1,24 @@
 #!usr/bin/env python3
-
-# @author /u/overlysalty
-
-# this script was designed to help developers dynamically pull RuneScape NPC drops from the wiki
-# it runs a flask server that receives input via GET requests and outputs directly to the page
-
-# If you want to use host your own version, take note that it runs on port 8000
-
-# If you don't want to host your own server, use the following request:
-# rs.d3x.me:8000/droptable?npc=NPC_NAME
-
-# Return Format:
-# [LINE 1]:      NPC_NAME       ,  NPC_IMAGE_LINK   
-# [LINES 2+]:    ITEM_IMAGE_LINK,  ITEM_NAME     ,  QUANTITY,  RARITY
-
-# If you don't want to item image links, add &icons=0 to the end of your request
-
+import argparse
+import requests
+import sys
 import os
 import datetime
-import requests
 from lxml import html
-from flask import Flask
-from flask import request
 
-# saves a shell script to stop the server if you decide to run it with 'nohup'
-with open('stop.sh', 'w') as pw:
-    pw.write('echo "Stopping Server..."\nkill ' + str(os.getpid()) + '\nrm stop.sh')
+# handle command line args
+parser = argparse.ArgumentParser()
 
-# formatted UTC time string for logging
-def utc_time():
-    return '[' + str(datetime.datetime.utcnow())[:-7] + ' (UTC)]: '
+parser.add_argument('-s', help='runs flask server on local computer to handle GET requests', action='store_true')
+parser.add_argument('-i', help='specify local ip to run on (default 0.0.0.0)', default='0.0.0.0')
+parser.add_argument('-p', help='specify port to run the server on (default 8000)', type=int, default=8000)
+parser.add_argument('-n', help='npc name to search (exits after results print, does not run server)')
+parser.add_argument('-o', help='specifies file output path (default NPC_NAME.dat)')
+parser.add_argument('-w', help='add this parameter if this script is being run from a windows machine', action='store_true')
+parser.add_argument('-c', help='if included, drop table output will not include icon image URL', action='store_true')
+
+args = parser.parse_args()
+
 
 # handles the wiki parsing, basically the backbone of this whole script
 def get_drop_table(boss, icons=True):
@@ -78,27 +67,61 @@ def get_drop_table(boss, icons=True):
     # return the drops
     return csv
 
-# create flask object
-app = Flask(__name__)
 
-# accept connections at HOSTNAME:8000/droptable
-@app.route('/droptable', methods=['GET'])
-def handle():
-    # log request IPs to see how much this is actually used by the community
-    with open('connections.dat', 'a') as fw:
-        fw.write(utc_time() + str(request.environ['REMOTE_ADDR']) + '\n')
-    r = ''
-    icon = True
-    if request.method == 'GET':
-        resp = request.args.getlist('npc')
-        icons = request.args.getlist('icons')
-        # if theres a value for ?icons=0, don't return icon links
-        if len(icons) == 1 and icons[0] == '0':
-            icon = False
-        # if there's a value for ?npc=, grab it and pass it to get_drop_table
-        if len(resp) == 1:
-            r = resp[0].capitalize()
-    return get_drop_table(r, icon)
+# if the user wants to run the server
+if args.s:
+    print('Attempting to run server on [%s:%d]' % (args.i, args.p))
+    try:
+        from flask import Flask
+        from flask import request
+    except:
+        print('Error importing Flask module.')
+        sys.exit()
+    if not args.w:
+        # saves a shell script to stop the server if you decide to run it with 'nohup'
+        with open('stop.sh', 'w') as pw:
+            pw.write('echo "Stopping Server..."\nkill ' + str(os.getpid()) + '\nrm stop.sh')
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000)
+    # formatted UTC time string for logging
+    def utc_time():
+        return '[' + str(datetime.datetime.utcnow())[:-7] + ' (UTC)]: '
+
+    # create flask object
+    app = Flask(__name__)
+     
+    # accept connections at HOSTNAME:8000/droptable
+    @app.route('/droptable', methods=['GET'])
+    def handle():
+        # log request IPs to see how much this is actually used by the community
+        with open('connections.dat', 'a') as fw:
+            fw.write(utc_time() + str(request.environ['REMOTE_ADDR']) + '\n')
+        r = ''
+        icon = True
+        if request.method == 'GET':
+            resp = request.args.getlist('npc')
+            icons = request.args.getlist('icons')
+            # if theres a value for ?icons=0, don't return icon links
+            if len(icons) == 1 and icons[0] == '0':
+                icon = False
+            # if there's a value for ?npc=, grab it and pass it to get_drop_table
+            if len(resp) == 1:
+                r = resp[0].capitalize()
+        return get_drop_table(r, icon)
+
+    if __name__ == '__main__':
+        app.run(host=args.i, port=args.p)
+elif args.n: # if the user only wants to run the script locally
+    print('Searching for drop table of NPC "%s"' % args.n)
+    # create default path based on whether or not the script is running on a windows system
+    path = str(os.getcwd().replace('\\','/')) + '/' + args.n + '.csv' if args.w else args.n + '.csv'
+    # change path if the user specified a custom path
+    if args.o:
+        path = args.o if '.csv' in args.o else args.o + '.csv'
+    # save data file
+    with open(path, 'w') as fw:
+        fw.write(get_drop_table(args.n.capitalize(), not args.c))
+        print('Saved file to "%s"' % path)
+else:
+    print('Please specify "-s" to run the server or "-n" to directly search for an npc')
+
+
